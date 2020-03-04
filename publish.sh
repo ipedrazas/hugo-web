4d2985318e08652a0f6b6c1b33117ae37e75e775
Ivan Pedrazas <ipedrazas@airbug.lan>
2020-03-04 21:08:33 +0000

    tidy up publish script

--- publish.sh
+++ publish.sh
@@ -8,24 +8,31 @@ TARGET_REPO="git@github.com:ipedrazas/${TARGET}.git"
 GITHUB_SHA=$(git rev-parse --verify HEAD)
 
 
-# clean up
-if [ -d "./public" ]; then 
-  rm -rf "./public"
-fi
-if [ -d "./dist" ]; then 
-  rm -rf "./dist"
-fi
+function cleanup(){
+  # clean up
+  if [ -d "./public" ]; then 
+    rm -rf "./public"
+  fi
+  if [ -d "./dist" ]; then 
+    rm -rf "./dist"
+  fi
+}
 
+cleanup
+
+# Clone the target repo
 git clone --depth=1 --single-branch --branch master ${TARGET_REPO} dist
 hugo --minify
 cp -r ./public/* ./dist
 
 pushd dist
 
-if [[ git status --porcelain --untracked-files=no ]]; then 
+# If there are any changes, commit & push
+if [[ $(git status --porcelain --untracked-files=no) ]]; then 
   git add .
   git commit -m "Automated deployment: $(date -R) ${GITHUB_SHA}"
   git push origin master
 fi
 
 popd
+cleanup
